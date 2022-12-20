813f8e8a51416033bc0d535b1e86ff98b877e16e
AndreaGallini <75275610+AndreaGallini@users.noreply.github.com>
2022-12-20 16:56:35 +0100

    Update index.blade.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid">
-        <h3><a href="{{ route('comics.create') }}">Lista Comics</a></h3>
+        <h3><a href="{{ route('comics.create') }}">Aggiungi Comic</a></h3>
         <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-2">
